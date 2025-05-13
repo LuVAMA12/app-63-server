fbbb044107b10875166db4205d303039d18b31e7
LuVAMA12 <jacqueraylucie@gmail.com>
2025-05-13 23:05:12 +0200

    import timeSlotRouter in index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import authRouter from './routes/auth.js'
 import itemRouter from './routes/items.js'
 import orderRouter from './routes/orders.js'
 import tableRouter from './routes/tables.js'
+import timeSlotRouter from './routes/timeSlots.js'
 import userRouter from './routes/users.js'
 
 const PORT = process.env.PORT || 4001
@@ -22,7 +23,7 @@ app.use('/images', express.static('public/images'))
 
 
 // We define the routes that can use in this server
-app.use('/api', userRouter, authRouter, itemRouter, orderRouter, tableRouter)
+app.use('/api', userRouter, authRouter, itemRouter, orderRouter, tableRouter, timeSlotRouter)
 
 // We connect the server to the db 
 connectDB() 
--- routes/timeSlots.js
+++ routes/timeSlots.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { getAllTimeSlots } from "../controllers/timeSlotController";
+import { getAllTimeSlots } from "../controllers/timeSlotController.js";
 
 const timeSlotRouter = Router()
 
 timeSlotRouter.get('slots', getAllTimeSlots)
+
+export default timeSlotRouter
