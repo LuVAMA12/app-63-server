c2814a00964644c94ac20c23a6d3854704bda9b4
luVAMA12 <jacqueraylucie@gmail.com>
2025-06-17 14:47:02 +0200

    fix bug import

---
+++ routes/admins.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken"
+import { getAdminByID, getAllAdmins, updateAdminByID } from "../controllers/adminController.js"
+import { createAdmin } from "../controllers/authController.js"
+import verifyAdmin from "../middleware/verifyUser.js"
+import { Router } from "express"
+import { checkOwner } from "../middleware/checkAutorizations.js"
+
+const adminRouter = Router()
+
+adminRouter.get('/admins',verifyAdmin, checkOwner, getAllAdmins)
+adminRouter.get('/profile', verifyAdmin, getAdminByID)
+adminRouter.put('/admin', verifyAdmin, updateAdminByID)
+
+
+export default adminRouter
